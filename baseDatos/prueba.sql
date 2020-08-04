671b9aa2194ac902fda293e630b043e3db9cb8c7
devArty <ghoul100p@gmail.com>
2020-08-04 02:15:16 -0500

    Base de datos

---
+++ baseDatos/prueba.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.21-MariaDB : Database - prueba
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `inventario` */
+
+DROP TABLE IF EXISTS `inventario`;
+
+CREATE TABLE `inventario` (
+  `idInventario` int(11) NOT NULL AUTO_INCREMENT,
+  `idComic` int(11) DEFAULT NULL,
+  `idSucursal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idInventario`),
+  KEY `idSucursal` (`idSucursal`),
+  CONSTRAINT `inventario_ibfk_1` FOREIGN KEY (`idSucursal`) REFERENCES `sucursales` (`idSucursal`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+
+/*Data for the table `inventario` */
+
+insert  into `inventario`(`idInventario`,`idComic`,`idSucursal`) values (15,82042,10),(16,80068,10),(17,80067,10),(18,49008,10),(19,49010,10),(20,82042,11),(21,81307,11),(22,80067,11),(23,71400,11),(24,49007,11),(25,49011,11),(26,37253,11),(27,37254,11);
+
+/*Table structure for table `sucursales` */
+
+DROP TABLE IF EXISTS `sucursales`;
+
+CREATE TABLE `sucursales` (
+  `idSucursal` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreSucursal` varchar(200) DEFAULT NULL,
+  `direccionSucursal` varchar(250) DEFAULT NULL,
+  `disponible` int(2) DEFAULT NULL,
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idSucursal`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+/*Data for the table `sucursales` */
+
+insert  into `sucursales`(`idSucursal`,`nombreSucursal`,`direccionSucursal`,`disponible`,`createDate`) values (10,'Primera sucursal','Calle Antonio Dovali',0,'2020-08-04 02:01:26'),(11,'Segunda Sucursal','Calle Benito Juarez',1,'2020-08-04 01:53:00');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
